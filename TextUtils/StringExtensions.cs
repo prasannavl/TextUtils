fe8025e21075f85a9171b56266b7b00855076c6c
Prasanna V. Loganathar <pvl@prasannavl.com>
2014-04-18 10:37:55 +0530

    Initial commit

---
+++ TextUtils/StringExtensions.cs
@@ -0,0 +1,52 @@
+﻿// Author: Prasanna V. Loganathar
+// Project: TextUtils
+// 
+// Copyright 2014 Launchark. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//  
+// Created: 1:49 AM 18-04-2014
+
+namespace TextUtils
+{
+    using System;
+    using System.Text;
+
+    public static class StringExtentions
+    {
+        public static string[] RepeatAsArray(this string source, int count)
+        {
+            var result = new string[count];
+            for (int i = 0; i < count; i++)
+            {
+                result[i] = source;
+            }
+            return result;
+        }
+
+        public static string Repeat(this string source, int count, bool addNewLine = false)
+        {
+            var sb = new StringBuilder(source.Length * count);
+            for (int i = 0; i < count; i++)
+            {
+                sb.Append(source);
+                if (addNewLine)
+                {
+                    sb.Append(Environment.NewLine);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ TextUtils/StringHelpers.cs
@@ -0,0 +1,34 @@
+﻿// Author: Prasanna V. Loganathar
+// Project: TextUtils
+// 
+// Copyright 2014 Launchark. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//  
+// Created: 1:54 AM 18-04-2014
+
+namespace TextUtils
+{
+    public static class StringHelpers
+    {
+        public static string[] RepeatAsArray(string text, int count)
+        {
+            return text.RepeatAsArray(count);
+        }
+
+        public static string Repeat(string text, int count, bool addNewLine = false)
+        {
+            return text.Repeat(count, addNewLine);
+        }
+    }
+}
